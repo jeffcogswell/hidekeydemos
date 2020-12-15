5fa46c4cd159bcffa3e1a629551e52d030164603
Jeff Cogswell <jeffcogs@gmail.com>
2020-12-15 17:58:39 -0500

    Demo 2 ready

--- awsencryptdemo/src/main/java/co/grandcircus/awsencryptdemo/MainController.java
+++ awsencryptdemo/src/main/java/co/grandcircus/awsencryptdemo/MainController.java
@@ -7,12 +7,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
+	// Don't lose track! We have two keys, the API key and the encryption key.
+	// For now we're hardcoding the encryptiong key so that the application.properties
+	// file doesn't have the API key shown in clear text.
+	
 	@Value("${db.apikey}")
-	private String apikey;
+	private String encryptedApikey;
+	
+	AESEncryptionDecryption aes = new AESEncryptionDecryption();	
+	private String encryptkey = "A4IIH983";
+	
+	private String privateApiKey = null;
+	private String getApiKey() {
+		if (privateApiKey == null) {
+			privateApiKey = aes.decrypt(encryptedApikey, encryptkey);
+		}
+		return privateApiKey;
+	}
 	
 	@GetMapping("/")
 	public String index() {
-		System.out.println("THE API KEY IS " + apikey);
+		
+		// First time we run it, generate the key so we can save that in the application.properties file instead
+		//System.out.println(aes.encrypt("ABC123", encryptkey));
+		
+		System.out.println("THE API KEY IS " + getApiKey());
 		return "index";
 	}
+
 }
